5f3be8f1e3b3a20b490439e97cd44f66e7dbdd75
khanal.sameer <57002785+Sameer725@users.noreply.github.com>
2024-01-05 07:25:58 +0545

    feat: authenticated in request context

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -21,7 +21,7 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileserver))
 	// mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
