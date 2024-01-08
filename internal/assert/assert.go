bc3bca8d0d7583e979c79212f07d415630dd6df7
khanal.sameer <57002785+Sameer725@users.noreply.github.com>
2024-01-08 07:41:18 +0545

    feat: add integration test and test db setup

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -18,3 +18,11 @@ func StringContains(t *testing.T, actual, expectedString string) {
 		t.Errorf("got: %q; expected to contain %q", actual, expectedString)
 	}
 }
+
+func NilError(t *testing.T, actual error) {
+	t.Helper()
+
+	if actual != nil {
+		t.Errorf("got: %v; expected: nil", actual)
+	}
+}
